37382a73725a4c12b70991c45da4671843130092
jahnavi <jahnavimudumbai@gmail.com>
2022-08-25 15:43:37 -0400

    Added Int C variable

--- src/jahnavi/intdeci.java
+++ src/jahnavi/intdeci.java
@@ -2,9 +2,10 @@ package jahnavi;
 
 public class intdeci {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		//declaire variables
 		double a = 3.6;	int b= 2;
+		int c = 10;
 		System.out.println( " a + b =" + ( a + b));
 		System.out.println( " a - b =" + ( a - b));
 		System.out.println( " a * b =" + ( a * b));
