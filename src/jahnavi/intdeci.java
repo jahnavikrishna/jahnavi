8210fe2736f034c6c8d166b77fed7ba82aaeb7ca
jahnavi <jahnavimudumbai@gmail.com>
2022-08-25 15:45:40 -0400

    removed int c

--- src/jahnavi/intdeci.java
+++ src/jahnavi/intdeci.java
@@ -5,7 +5,6 @@ public class intdeci {
 		public static void main(String[] args) {
 		//declaire variables
 		double a = 3.6;	int b= 2;
-		int c = 10;
 		System.out.println( " a + b =" + ( a + b));
 		System.out.println( " a - b =" + ( a - b));
 		System.out.println( " a * b =" + ( a * b));
